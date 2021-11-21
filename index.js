e270464d13348bf6645a8ba25998725740df90ab
Alex Martinez <alexkmartinez77@gmail.com>
2021-11-21 10:47:26 -0800

    Creates callback route where we will access the token sent by google.

--- index.js
+++ index.js
@@ -25,6 +25,11 @@ app.get(
   })
 );
 
+app.get(
+  '/auth/google/callback', 
+  passport.authenticate('google')
+);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
 
